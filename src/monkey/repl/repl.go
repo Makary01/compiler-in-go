88261c748253673356f0ebcf5f4457bb87241c5e
Makary01 <makary.bortnowski@gmail.com>
2024-07-21 15:18:24 +0200

    use evaluator in repl

--- src/monkey/repl/repl.go
+++ src/monkey/repl/repl.go
@@ -6,6 +6,7 @@ import (
     "io"
     "github.com/Makary01/interpreter-in-go/src/monkey/lexer"
     "github.com/Makary01/interpreter-in-go/src/monkey/parser"
+    "github.com/Makary01/interpreter-in-go/src/monkey/evaluator"
 )
 
 const PROMPT = ">> "
@@ -30,8 +31,11 @@ func Start(in io.Reader, out io.Writer) {
             continue
         }
 
-        io.WriteString(out, program.String())
-        io.WriteString(out, "\n")
+        evaluated := evaluator.Eval(program)
+        if evaluated != nil {
+            io.WriteString(out, evaluated.Inspect())
+            io.WriteString(out, "\n")
+        }
     }
 }
 
