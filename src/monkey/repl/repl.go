229945c1d434c5cb5a7f1f4ac9772899efa9b3bf
Makary01 <makary.bortnowski@gmail.com>
2024-10-12 12:44:31 +0200

    add support for identifiers

--- src/monkey/repl/repl.go
+++ src/monkey/repl/repl.go
@@ -7,12 +7,14 @@ import (
     "github.com/Makary01/interpreter-in-go/src/monkey/lexer"
     "github.com/Makary01/interpreter-in-go/src/monkey/parser"
     "github.com/Makary01/interpreter-in-go/src/monkey/evaluator"
+    "github.com/Makary01/interpreter-in-go/src/monkey/object"
 )
 
 const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
     scanner := bufio.NewScanner(in)
+    env := object.NewEnvironment()
 
     for {
         fmt.Fprintf(out, PROMPT)
@@ -31,7 +33,7 @@ func Start(in io.Reader, out io.Writer) {
             continue
         }
 
-        evaluated := evaluator.Eval(program)
+        evaluated := evaluator.Eval(program, env)
         if evaluated != nil {
             io.WriteString(out, evaluated.Inspect())
             io.WriteString(out, "\n")
