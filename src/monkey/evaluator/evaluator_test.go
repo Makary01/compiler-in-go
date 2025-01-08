1b1fd3a5c2df40e76007aadf838c5263cde0fe77
Makary01 <makary.bortnowski@gmail.com>
2025-01-08 18:37:12 +0100

    add enclosed environments for functions

--- src/monkey/evaluator/evaluator_test.go
+++ src/monkey/evaluator/evaluator_test.go
@@ -243,6 +243,36 @@ func TestFunctionObject(t *testing.T) {
     }
 }
 
+func TestFunctionApplication(t *testing.T) {
+    tests := []struct {
+        input string
+        expected int64
+    }{
+        {"let identity = fn(x) {x;}; identity(5);", 5},
+        {"let identity = fn(x) {return x;}; identity(5);", 5},
+        {"let double = fn(x) {x * 2;}; double(5);", 10},
+        {"let add = fn(x, y) {x + y;}; add(5, 6);", 11},
+        {"let add = fn(x, y) {x + y;}; add(5 + 6, add(7, 8));", 26},
+        {"fn(x) {x;}(5)", 5},
+    }
+
+    for _, tt := range tests {
+        testIntegerObject(t, testEval(tt.input), tt.expected)
+    }
+}
+
+func TestClosures(t *testing.T) {
+    input :=`
+    let newAdder = fn(x) {
+        fn(y) {x + y};
+    };
+
+    let addTwo = newAdder(2);
+    addTwo(3);
+    `
+    testIntegerObject(t, testEval(input), 5)
+}
+
 func testNullObject(t *testing.T, obj object.Object) bool {
     if obj != NULL {
         t.Errorf("object is not NULL. got=%T (%+v)", obj, obj)
